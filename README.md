f0b265dfd53130ba58405fcd4748c5cbba3b6307
daslu <daniel@madlan.co.il>
2019-01-14 23:54:23 +0200

    initial commit: some notes about machine learning dataflow

---
+++ README.md
@@ -0,0 +1,7 @@
+# notes
+
+various notes
+
+[machine learning dataflow](http://viewer.gorilla-repl.org/view.html?source=github&user=daslu&repo=notes&path=src/notes/machine_learning_workflow.clj)
+
+
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject notes "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [alembic "0.3.2"]
+                 [org.clojars.benfb/gorilla-repl "0.5.3"]
+                 [gorilla-plot "0.1.4"]]
+  :plugins [[org.clojars.benfb/lein-gorilla "0.5.3"]])
